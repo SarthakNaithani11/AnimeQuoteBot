8e8a2c51465d28fa4dc992be911a23f8832bd912
Sarthak Naithani <sarthaknaithani4545@gmail.com>
2025-06-28 17:33:02 +0530

    initial commit

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 require('dotenv').config({ path: './.env' });
 const { TwitterApi } = require('twitter-api-v2');
 const axios = require('axios');
+const express = require('express');
+
+let app = express();
+app.use(express.json());
 
 const url = "https://waifu.it/api/v4/quote";
+const port = process.env.PORT || 3000;
 
 
 const twitterClient = new TwitterApi({
@@ -43,4 +48,16 @@ async function tweetQuote() {
     console.error("❌ Failed to tweet:", err.response?.data || err.message);
   }
 }
-tweetQuote();
+app.get('/', async (req, res) => {
+  try {
+    tweetQuote();
+    res.status(200).send("Quote tweeted successfully!");
+  } catch (err) {
+    console.error("Error in / route:", err.message);
+    res.status(500).send("Failed to tweet quote.");
+  }
+})
+
+App.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
